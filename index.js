563a2be0106e6bad9153ffb6b43fb0006c9ee854
hormakery <hormakery@gmail.com>
2023-09-16 18:07:50 +0100

    clear unused code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const body = document.body;
     body.classList.toggle("dark-mode");
   }
-  
+
   //to get the value of the toggle button and call the toggleDarkMode function
   const ToggleBtn = document.getElementById("mode-toggle");
   ToggleBtn.addEventListener("click", toggleDarkMode);
@@ -25,57 +25,4 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   //to get the value of the button clicked and show it in the input box and evaluate the string
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const buttonText = button.innerHTML;
-
-      switch (buttonText) {
-        case "=":
-          try {
-            string = eval(string);
-            if (isNaN(string) || !isFinite(string)) {
-              input.value = "Error";
-            } else {
-              let output = document.createElement("p");
-              output.innerHTML = string;
-              input.parentNode.appendChild(output);
-              output.style.fontSize = "2rem";
-              output.style.fontWeight = "bold";
-              output.style.color = "white";
-              output.style.textAlign = "right";
-              output.style.marginRight = "1rem";
-            }
-          } catch (error) {
-            input.value = "Error";
-          }
-          break;
-
-        //to set the value of the toggle button to null
-        case "Dark Mode":
-          input.value = null;
-          break;
-
-        //to clear the input box
-        case "c":
-          string = "";
-          input.value = string;
-          let output = document.querySelector("p");
-          if (output) {
-            output.remove();
-          }
-          break;
-
-        //to delete the last character
-        case "DEL":
-          string = string.slice(0, -1);
-          input.value = string;
-          break;
-
-        default:
-          string += buttonText;
-          input.value = string;
-          break;
-      }
-    });
-  });
 });
